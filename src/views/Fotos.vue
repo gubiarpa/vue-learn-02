61eb756e7c153e0e9ee391ea96641a5935837064
gubiarpa <billy.arredondo@pucp.pe>
2020-07-16 01:41:05 -0500

    Generating routes from the data
    Routes were generated from the data with the router-link tag. this.$router methods were also used to travel in the application's browsing history.

--- src/views/Fotos.vue
+++ src/views/Fotos.vue
@@ -1,7 +1,14 @@
 <template>
     <div>
         <h1>Galería de Fotos</h1>
+        <router-link :to="{ name: 'Fotos', params: { id: item } }"
+            v-for="(item, index) of fotosArreglo" :key="index">
+            <button>Foto {{item}}</button>
+        </router-link>
         <Foto />
+        <button @click="anterior">Anterior</button>
+        <button @click="home">Home</button>
+        <button @click="siguiente">Siguiente</button>
     </div>
 </template>
 
@@ -11,6 +18,22 @@ import Foto from '@/components/Fotografia.vue'
 export default {
     components: {
         Foto
-    }
+    },
+    data(){
+        return {
+            fotosArreglo: [1, 2, 3]
+        }
+    },
+    methods: {
+        home(){
+            this.$router.push('/');
+        },
+        anterior(){
+            this.$router.go(-1);
+        },
+        siguiente(){
+            this.$router.go(1)
+        }
+    },
 }
 </script>
